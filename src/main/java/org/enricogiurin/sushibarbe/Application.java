55f7252ab000d65e3139991a8d07ec4bc187ea98
egch <enricogiurin@gmail.com>
2017-07-06 18:14:22 +0200

    springboot draft

---
+++ src/main/java/org/enricogiurin/sushibarbe/Application.java
@@ -0,0 +1,15 @@
+package org.enricogiurin.sushibarbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by enrico on 2/12/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
