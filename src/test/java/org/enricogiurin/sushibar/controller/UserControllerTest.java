1f34dc891b7a03bc9248c10afbc381842ee0c7ae
egch <enricogiurin@gmail.com>
2017-07-16 01:46:29 +0200

    added test of Controllers

---
+++ src/test/java/org/enricogiurin/sushibar/controller/UserControllerTest.java
@@ -0,0 +1,77 @@
+package org.enricogiurin.sushibar.controller;
+
+import org.enricogiurin.sushibar.Application;
+import org.enricogiurin.sushibar.model.User;
+import org.enricogiurin.sushibar.model.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+/**
+ * Created by enrico on 7/16/17.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+public class UserControllerTest {
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+        List<User> users = new ArrayList<>();
+        User user = new User();
+        user.setUsername("aa");
+        user.setEmail("aa@comp.org");
+        users.add(user);
+        user = new User();
+        user.setUsername("bb");
+        user.setEmail("bb@comp.org");
+        users.add(user);
+        when(userRepository.activeUsers()).thenReturn(users);
+
+    }
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    public void activeUsers() throws Exception {
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].username", is("aa")))
+                .andExpect(jsonPath("$[0].email", is("aa@comp.org")))
+                .andExpect(jsonPath("$[1].username", is("bb")))
+                .andExpect(jsonPath("$[1].email", is("bb@comp.org")));
+
+    }
+
+}
