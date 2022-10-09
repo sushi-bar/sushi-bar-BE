440d13a263c8b4c5ad4ed25a824930ba2380c096
Enrico Giurin <enricogiurin@gmail.com>
2022-10-09 04:17:13 +0200

    issue_11 - Basic kafka publishing message

--- src/main/java/org/virtualsushibar/backend/MainApplication.java
+++ src/main/java/org/virtualsushibar/backend/MainApplication.java
@@ -1,13 +1,13 @@
-package org.virtualsushibar.rest;
+package org.virtualsushibar.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
---
+++ src/main/java/org/virtualsushibar/backend/kafka/KafkaProducerConfig.java
@@ -0,0 +1,40 @@
+package org.virtualsushibar.backend.kafka;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+
+    private final String bootstrapAddress;
+
+    public KafkaProducerConfig(@Value(value = "${kafka.bootstrapAddress}")String bootstrapAddress) {
+        this.bootstrapAddress = bootstrapAddress;
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, "20971520");
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
