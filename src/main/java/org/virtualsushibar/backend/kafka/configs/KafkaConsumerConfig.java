439d083fb02dcd03f9dfc3f2ad0dc328c7c6db12
sarthak-97 <sarthakgupta115@gmail.com>
2022-10-28 19:13:47 +0530

    removed unused imports

--- src/main/java/org/virtualsushibar/backend/kafka/configs/KafkaConsumerConfig.java
+++ src/main/java/org/virtualsushibar/backend/kafka/configs/KafkaConsumerConfig.java
@@ -3,18 +3,12 @@ package org.virtualsushibar.backend.kafka.configs;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-
-
 import java.util.HashMap;
 import java.util.Map;
 @Configuration
--- src/main/java/org/virtualsushibar/backend/kafka/configs/KafkaProducerConfig.java
+++ src/main/java/org/virtualsushibar/backend/kafka/configs/KafkaProducerConfig.java
@@ -3,13 +3,11 @@ package org.virtualsushibar.backend.kafka.configs;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-
 import java.util.HashMap;
 import java.util.Map;
 
