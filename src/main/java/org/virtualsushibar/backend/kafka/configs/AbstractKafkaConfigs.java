4d532297ae6dcfcce7cced30e356ebd22e95ad6e
sarthak-97 <sarthakgupta115@gmail.com>
2022-10-28 18:57:39 +0530

    constructor injection added

--- src/main/java/org/virtualsushibar/backend/kafka/configs/AbstractKafkaConfigs.java
+++ src/main/java/org/virtualsushibar/backend/kafka/configs/AbstractKafkaConfigs.java
@@ -1,18 +1,20 @@
 package org.virtualsushibar.backend.kafka.configs;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
-@RequiredArgsConstructor
 @Configuration
 public class AbstractKafkaConfigs {
 
+    public AbstractKafkaConfigs(@Value(value = "${application.topic.name}") String topic,
+                                @Value(value = "${kafka.bootstrapAddress}") String bootstrapAddress){
 
-    @Value(value = "${application.topic.name}")
-    private  String topic;
-    @Value(value = "${kafka.bootstrapAddress}")
+        this.topic=topic;
+        this.bootstapAddress=bootstrapAddress;
+    }
+
+   private  String topic;
     private  String bootstapAddress;
 }
