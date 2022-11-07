511bf37ce6a74db4e000708060149e1f33491963
Enrico Giurin <enricogiurin@gmail.com>
2022-11-07 01:09:23 +0100

    issue_20 - draft TestContainers

---
+++ sushibar-backend-kafka/src/test/java/org/virtualsushibar/backend/TestContainers.java
@@ -0,0 +1,26 @@
+package org.virtualsushibar.backend;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+class TestContainers {
+    private static final DockerImageName KAFKA_TEST_IMAGE = DockerImageName.parse("confluentinc/cp-kafka:latest");
+    static String bootstrapServers;
+
+    @Test
+    void loadKafka(){
+        assertThat(bootstrapServers).isNotNull();
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        try (KafkaContainer kafka = new KafkaContainer(KAFKA_TEST_IMAGE)) {
+            kafka.start();
+            bootstrapServers = kafka.getBootstrapServers();
+        }
+    }
+}
