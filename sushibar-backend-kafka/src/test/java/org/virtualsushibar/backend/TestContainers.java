6bccd0dd0a446054d1bc910a6043f5af7e09578b
sarthak-97 <sarthakgupta115@gmail.com>
2022-11-08 17:19:01 +0530

    added log in catch

--- sushibar-backend-kafka/src/test/java/org/virtualsushibar/backend/TestContainers.java
+++ sushibar-backend-kafka/src/test/java/org/virtualsushibar/backend/TestContainers.java
@@ -1,5 +1,6 @@
 package org.virtualsushibar.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.KafkaContainer;
@@ -7,7 +8,8 @@ import org.testcontainers.utility.DockerImageName;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-class TestContainers {
+@Slf4j
+public class TestContainers {
     private static final DockerImageName KAFKA_TEST_IMAGE = DockerImageName.parse("confluentinc/cp-kafka");
     static String bootstrapServers;
 
@@ -21,6 +23,9 @@ class TestContainers {
         try (KafkaContainer kafka = new KafkaContainer(KAFKA_TEST_IMAGE)) {
             kafka.start();
             bootstrapServers = kafka.getBootstrapServers();
+        }
+        catch (Exception e){
+            log.error(e.getMessage());
         }
     }
 }
