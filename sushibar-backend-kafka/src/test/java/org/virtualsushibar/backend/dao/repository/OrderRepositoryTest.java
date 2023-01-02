cbc9ecfffbee3fdcef445983e3b67e9f4d31d1b4
Enrico Giurin <enricogiurin@gmail.com>
2023-01-02 01:57:15 +0100

    fixing test container issue

--- sushibar-backend-kafka/src/test/java/org/virtualsushibar/backend/dao/repository/OrderRepositoryTest.java
+++ sushibar-backend-kafka/src/test/java/org/virtualsushibar/backend/dao/repository/OrderRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -23,6 +25,11 @@ class OrderRepositoryTest {
 
     @Container
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
 
     @Resource
