08051d685868d72d48f4b30733e184b83d30de86
SARTHAK GUPTA <sarthakgupta115@gmail.com>
2023-01-07 10:11:50 +0530

    40 create cook ms submodule (#42)
    * cook submodule added
    
    * COOK MS added

---
+++ sushibar-backend-cook/src/main/java/org/virtualsushibar/backend/CookApplication.java
@@ -0,0 +1,13 @@
+package org.virtualsushibar.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookApplication.class, args);
+	}
+
+}
