16393e8c97f56f1a287566927ff91344abea00fe
Enrico Giurin <enricogiurin@gmail.com>
2023-01-15 22:47:42 +0100

    issue_43 - fixing integration tests & loggers

--- sushibar-backend-cook/src/test/java/org/virtualsushibar/backend/cook/CookApplicationTests.java
+++ sushibar-backend-cook/src/test/java/org/virtualsushibar/backend/cook/CookApplicationTests.java
@@ -1,10 +1,22 @@
 package org.virtualsushibar.backend.cook;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
 
 @SpringBootTest
+@Testcontainers
+@DisabledIfSystemProperty(named = "target.env", matches = "circleci")
 class CookApplicationTests {
+	private static final DockerImageName KAFKA_TEST_IMAGE = DockerImageName.parse("confluentinc/cp-kafka:latest");
+
+	@Container
+	static KafkaContainer kafkaContainer= new KafkaContainer(KAFKA_TEST_IMAGE);
 
 	@Test
 	void contextLoads() {
