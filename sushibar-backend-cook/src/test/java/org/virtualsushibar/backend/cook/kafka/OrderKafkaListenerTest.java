51caf734d24c84f2eb3e2d1696a484d7ab4334a6
Enrico Giurin <enricogiurin@gmail.com>
2024-11-06 02:13:27 +0100

    issue_80 - enabling ProcessedOrderKafkaListenerTest

--- sushibar-backend-cook/src/test/java/org/virtualsushibar/backend/cook/kafka/OrderKafkaListenerTest.java
+++ sushibar-backend-cook/src/test/java/org/virtualsushibar/backend/cook/kafka/OrderKafkaListenerTest.java
@@ -1,12 +1,13 @@
 package org.virtualsushibar.backend.cook.kafka;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.verify;
 
 import java.time.Duration;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +16,10 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
@@ -31,10 +35,14 @@ import org.virtualsushibar.backend.cook.CookService;
 @Testcontainers
 class OrderKafkaListenerTest {
 
+  private static final String CONFLUENT_VERSION = "7.6.1";
+
+
+  private static final Network NETWORK = Network.newNetwork();
+
   @Container
-  static final KafkaContainer kafka = new KafkaContainer(
-      DockerImageName.parse("confluentinc/cp-kafka:7.3.3")
-  );
+  static final KafkaContainer KAFKA_CONTAINER = new KafkaContainer(
+      DockerImageName.parse("confluentinc/cp-kafka:" + CONFLUENT_VERSION)).withNetwork(NETWORK);
   private static final String ORDER_ID = "1";
   @MockBean
   CookService cookService;
@@ -43,28 +51,49 @@ class OrderKafkaListenerTest {
   @Autowired
   private KafkaTemplate<String, Order> kafkaTemplate;
 
-  @DynamicPropertySource
-  static void overrideProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+  @Container
+  private static final GenericContainer<?> SCHEMA_REGISTRY =
+      new GenericContainer<>(
+          DockerImageName.parse("confluentinc/cp-schema-registry:" + CONFLUENT_VERSION))
+          .withNetwork(NETWORK)
+          .withExposedPorts(8081)
+          .withEnv("SCHEMA_REGISTRY_HOST_NAME", "schema-registry")
+          .withEnv("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:8081")
+          .withEnv("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS",
+              "PLAINTEXT://" + KAFKA_CONTAINER.getNetworkAliases().get(0) + ":9092")
+          .waitingFor(Wait.forHttp("/subjects").forStatusCode(200));
+
+  private static String getSchemaRegistryUrl() {
+    return """
+        http://%s:%d
+        """.formatted(SCHEMA_REGISTRY.getHost(), SCHEMA_REGISTRY.getFirstMappedPort());
   }
 
-  //TODO - fix it
+  @DynamicPropertySource
+  static void overrideProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.kafka.bootstrap-servers", KAFKA_CONTAINER::getBootstrapServers);
+    registry.add("spring.kafka.properties.schema.registry.url",
+        OrderKafkaListenerTest::getSchemaRegistryUrl);
+  }
+
   @Test
-  @Disabled
   void consumeOrder() {
     Order order = Order.newBuilder()
         .setOrderId(ORDER_ID)
         .setAmount(1)
         .setMeal("PIZZA")
         .build();
+    ArgumentCaptor<Order> orderArgumentCaptor = ArgumentCaptor.forClass(Order.class);
 
     kafkaTemplate.send(topic, "", order);
     await()
         .pollInterval(Duration.ofSeconds(1))
-        .atMost(2, SECONDS)
+        .atMost(3, SECONDS)
         .untilAsserted(() -> {
-          verify(cookService).processOrder(order);
+          verify(cookService).processOrder(orderArgumentCaptor.capture());
         });
+    Order value = orderArgumentCaptor.getValue();
+    assertThat(value.getOrderId()).isEqualTo(ORDER_ID);
   }
 }
 
