a72ed5d16e9f8f8c124dc602f470fcb81a592e69
Enrico Giurin <egch@users.noreply.github.com>
2024-11-06 02:58:16 +0100

    issue_82 - disabling security when local profile is active (#83)

--- sushibar-backend-app/src/main/java/org/virtualsushibar/backend/app/conf/SecurityConfig.java
+++ sushibar-backend-app/src/main/java/org/virtualsushibar/backend/app/conf/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.virtualsushibar.backend.app.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
   @Bean
+  @Profile("!local")
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests((authz) -> authz
@@ -18,4 +20,20 @@ public class SecurityConfig {
 
     return http.build();
   }
+
+  /**
+   * This disabled the authentication/authorization when the profile local is active
+   */
+  @Bean
+  @Profile("local")
+  public SecurityFilterChain filterChain_disabled(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests((authz) -> authz
+            .anyRequest().permitAll()  // Allow all requests without authentication
+        )
+        .csrf().disable()              // Optionally disable CSRF for non-production environments
+        .oauth2Login().disable();      // Disable OAuth2 login if not needed locally
+    return http.build();
+  }
+
 }
